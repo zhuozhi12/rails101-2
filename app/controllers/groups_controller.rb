2c562889338c1e527f2e57e48e69ae0ce1c543ee
zhuozhi12 <feibou@126.com>
2017-03-31 22:37:22 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+    before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -10,11 +11,6 @@ before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destr
     end
 
     def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
   end
 
 
@@ -35,34 +31,30 @@ before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destr
 
 
   def update
-   @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
 
-   if current_user != @group.user
-     redirect_to root_path, alert: "You have no permission."
-   end
-
-   if @group.update(group_params)
-     redirect_to groups_path, notice: "Update Success"
-   else
-     render :edit
-   end
- end
-
- def destroy
-   @group = Group.find(params[:id])
-
-   if current_user != @group.user
-     redirect_to root_path, alert: "You have no permission."
-   end
-
-   @group.destroy
-   redirect_to groups_path, alert: "Group deleted"
- end
-
+  def destroy
+    @group.destroy
+    redirect_to groups_path, alert: "Group deleted"
+  end
+  
    private
 
-   def group_params
-     params.require(:group).permit(:title, :description)
-   end
+   def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 
 end
